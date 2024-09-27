35c0c9adcea6722b393f946e4979522ac1073d87
Moe Chaieb <moe@timeoff.audio>
2024-09-27 11:59:43 +0200

    Remove scanner worker message thread condition

--- src/scanner/Worker.h
+++ src/scanner/Worker.h
@@ -1,155 +1,143 @@
 #pragma once
-#include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_events/juce_events.h>
 
-namespace timeoffaudio {
-    namespace scanner {
-        class Worker final : private juce::ChildProcessWorker, private juce::AsyncUpdater {
-        public:
-            Worker()
-                : logger (juce::FileLogger::createDateStampedLogger ("time off audio", "PluginScanner", ".log", "")) {
-                formatManager.addFormat (new juce::VST3PluginFormat());
+namespace timeoffaudio::scanner {
+    class Worker final : private juce::ChildProcessWorker, private juce::AsyncUpdater {
+    public:
+        Worker() : logger (juce::FileLogger::createDateStampedLogger ("time off audio", "PluginScanner", ".log", "")) {
+            formatManager.addFormat (new juce::VST3PluginFormat());
 #if JUCE_MAC
-                formatManager.addFormat (new juce::AudioUnitPluginFormat());
+            formatManager.addFormat (new juce::AudioUnitPluginFormat());
 #endif
-                logger->logMessage ("[timeoffaudio::scanner::Worker] Constructor completed.");
+            logger->logMessage ("[timeoffaudio::scanner::Worker] Constructor completed.");
+        }
+
+        bool initialiseFromCommandLine (const juce::String& commandLine,
+            const juce::String& commandLineUniqueID,
+            int timeoutMs = 0) {
+            logger->logMessage ("[timeoffaudio::scanner::Worker] Initialised from command line.");
+            logger->logMessage ("[timeoffaudio::scanner::Worker] With arguments: commandLine:" + commandLine
+                                + ", commandLineUniqueID:" + commandLineUniqueID
+                                + ", timeoutMs:" + juce::String (timeoutMs));
+            const bool result =
+                juce::ChildProcessWorker::initialiseFromCommandLine (commandLine, commandLineUniqueID, timeoutMs);
+
+            if (result) {
+                logger->logMessage (
+                    "[timeoffaudio::scanner::Worker] juce::ChildProcessWorker::initialiseFromCommandLine successful.");
+            } else {
+                logger->logMessage (
+                    "[timeoffaudio::scanner::Worker] juce::ChildProcessWorker::initialiseFromCommandLine failed.");
             }
 
-            bool initialiseFromCommandLine (const juce::String& commandLine,
-                const juce::String& commandLineUniqueID,
-                int timeoutMs = 0) {
-                logger->logMessage ("[timeoffaudio::scanner::Worker] Initialised from command line.");
-                logger->logMessage ("[timeoffaudio::scanner::Worker] With arguments: commandLine:" + commandLine
-                                    + ", commandLineUniqueID:" + commandLineUniqueID
-                                    + ", timeoutMs:" + juce::String (timeoutMs));
-                bool result =
-                    juce::ChildProcessWorker::initialiseFromCommandLine (commandLine, commandLineUniqueID, timeoutMs);
+            return result;
+        }
 
-                if (result) {
-                    logger->logMessage (
-                        "[timeoffaudio::scanner::Worker] juce::ChildProcessWorker::initialiseFromCommandLine successful.");
-                } else {
-                    logger->logMessage (
-                        "[timeoffaudio::scanner::Worker] juce::ChildProcessWorker::initialiseFromCommandLine failed.");
+    private:
+        void handleMessageFromCoordinator (const juce::MemoryBlock& mb) override {
+            logger->logMessage ("[timeoffaudio::scanner::Worker] Received message from coordinator, of size "
+                                + juce::String (mb.getSize()));
+
+            if (mb.isEmpty()) return;
+
+            if (!doScan (mb)) {
+                {
+                    const std::lock_guard<std::mutex> lock (mutex);
+                    pendingBlocks.emplace (mb);
                 }
 
-                return result;
+                triggerAsyncUpdate();
             }
+        }
 
-        private:
-            void handleMessageFromCoordinator (const juce::MemoryBlock& mb) override {
-                logger->logMessage ("[timeoffaudio::scanner::Worker] Received message from coordinator, of size "
-                                    + juce::String (mb.getSize()));
+        void handleConnectionLost() override {
+            logger->logMessage ("[timeoffaudio::scanner::Worker] Connection lost.");
+            juce::JUCEApplicationBase::quit();
+        }
 
-                if (mb.isEmpty()) return;
+        void handleAsyncUpdate() override {
+            for (;;) {
+                const auto block = [&]() -> juce::MemoryBlock {
+                    const std::lock_guard lock (mutex);
 
-                if (!doScan (mb)) {
-                    {
-                        const std::lock_guard<std::mutex> lock (mutex);
-                        pendingBlocks.emplace (mb);
-                    }
+                    if (pendingBlocks.empty()) return {};
 
-                    triggerAsyncUpdate();
-                }
-            }
-
-            void handleConnectionLost() override {
-                logger->logMessage ("[timeoffaudio::scanner::Worker] Connection lost.");
-                juce::JUCEApplicationBase::quit();
-            }
-
-            void handleAsyncUpdate() override {
-                for (;;) {
-                    const auto block = [&]() -> juce::MemoryBlock {
-                        const std::lock_guard<std::mutex> lock (mutex);
-
-                        if (pendingBlocks.empty()) return {};
-
-                        auto out = std::move (pendingBlocks.front());
-                        pendingBlocks.pop();
-                        return out;
-                    }();
-
-                    if (block.isEmpty()) return;
-
-                    doScan (block);
-                }
-            }
-
-            bool doScan (const juce::MemoryBlock& block) {
-                juce::MemoryInputStream stream { block, false };
-                const auto formatName = stream.readString();
-                const auto identifier = stream.readString();
-
-                logger->logMessage ("[timeoffaudio::scanner::Worker] doScan: formatName: " + formatName
-                                    + ", identifier: " + identifier);
-
-                juce::PluginDescription pd;
-                pd.fileOrIdentifier = identifier;
-                pd.uniqueId = pd.deprecatedUid = 0;
-
-                const auto matchingFormat = [&]() -> juce::AudioPluginFormat* {
-                    for (auto* format : formatManager.getFormats())
-                        if (format->getName() == formatName) return format;
-
-                    return nullptr;
+                    auto out = std::move (pendingBlocks.front());
+                    pendingBlocks.pop();
+                    return out;
                 }();
 
-                if (matchingFormat == nullptr) {
-                    logger->logMessage ("[timeoffaudio::scanner::Worker] doScan failed. Did not find matching format");
-                    return false;
-                }
+                if (block.isEmpty()) return;
 
-                if (!juce::MessageManager::getInstance()->isThisTheMessageThread()
-                    && !matchingFormat->requiresUnblockedMessageThreadDuringCreation (pd)) {
-                    logger->logMessage (
-                        "[timeoffaudio::scanner::Worker] doScan failed. Requires unblocked message thread (format: "
-                        + matchingFormat->getName() + ")");
-                    return false;
-                }
+                doScan (block);
+            }
+        }
 
-                if(!matchingFormat->fileMightContainThisPluginType(identifier)) {
-                    logger->logMessage ("[timeoffaudio::scanner::Worker] doScan failed. fileMightContainThisPluginType returned false for identifier: " + identifier);
-                    return false;
-                }
+        bool doScan (const juce::MemoryBlock& block) {
+            juce::MemoryInputStream stream { block, false };
+            const auto formatName = stream.readString();
+            const auto identifier = stream.readString();
 
-                // if(matchingFormat->getName() == "VST3") {
-                //     juce::VST3PluginFormat* vst3Format = static_cast<juce::VST3PluginFormat*>(matchingFormat);
-                // }
+            logger->logMessage (
+                "[timeoffaudio::scanner::Worker] doScan: formatName: " + formatName + ", identifier: " + identifier);
 
-                juce::OwnedArray<juce::PluginDescription> results;
-                matchingFormat->findAllTypesForFile (results, identifier);
+            juce::PluginDescription pd;
+            pd.fileOrIdentifier = identifier;
+            pd.uniqueId = pd.deprecatedUid = 0;
 
-                if(results.isEmpty()) {
-                    logger->logMessage ("[timeoffaudio::scanner::Worker] [Warning] No plugin descriptions found for identifier: " + identifier);
-                }
+            const auto matchingFormat = [&]() -> juce::AudioPluginFormat* {
+                for (auto* format : formatManager.getFormats())
+                    if (format->getName() == formatName) return format;
 
-                sendPluginDescriptions (results);
+                return nullptr;
+            }();
 
-                return true;
+            if (matchingFormat == nullptr) {
+                logger->logMessage ("[timeoffaudio::scanner::Worker] doScan failed. Did not find matching format");
+                return false;
             }
 
-            void sendPluginDescriptions (const juce::OwnedArray<juce::PluginDescription>& results) {
-                juce::XmlElement xml ("LIST");
-
-                for (const auto& desc : results) xml.addChildElement (desc->createXml().release());
-
-                const auto str = xml.toString();
-                sendMessageToCoordinator ({ str.toRawUTF8(), str.getNumBytesAsUTF8() });
-
+            if (!matchingFormat->fileMightContainThisPluginType (identifier)) {
                 logger->logMessage (
-                    "[timeoffaudio::scanner::Worker] Sent plugin descriptions to coordinator. Message: " + str);
+                    "[timeoffaudio::scanner::Worker] doScan failed. fileMightContainThisPluginType returned false for identifier: "
+                    + identifier);
+                return false;
             }
 
-            std::mutex mutex;
-            std::queue<juce::MemoryBlock> pendingBlocks;
+            juce::OwnedArray<juce::PluginDescription> results;
+            matchingFormat->findAllTypesForFile (results, identifier);
 
-            // After construction, this will only be accessed by doScan so there's no need
-            // to worry about synchronisation.
-            juce::AudioPluginFormatManager formatManager;
-            juce::FileLogger* logger;
-        };
+            if (results.isEmpty()) {
+                logger->logMessage (
+                    "[timeoffaudio::scanner::Worker] [Warning] No plugin descriptions found for identifier: "
+                    + identifier);
+            }
 
-    }
+            sendPluginDescriptions (results);
+
+            return true;
+        }
+
+        void sendPluginDescriptions (const juce::OwnedArray<juce::PluginDescription>& results) {
+            juce::XmlElement xml ("LIST");
+
+            for (const auto& desc : results) xml.addChildElement (desc->createXml().release());
+
+            const auto str = xml.toString();
+            sendMessageToCoordinator ({ str.toRawUTF8(), str.getNumBytesAsUTF8() });
+
+            logger->logMessage (
+                "[timeoffaudio::scanner::Worker] Sent plugin descriptions to coordinator. Message: " + str);
+        }
+
+        std::mutex mutex;
+        std::queue<juce::MemoryBlock> pendingBlocks;
+
+        // After construction, this will only be accessed by doScan so there's no need
+        // to worry about synchronisation.
+        juce::AudioPluginFormatManager formatManager;
+        juce::FileLogger* logger;
+    };
+
 }
